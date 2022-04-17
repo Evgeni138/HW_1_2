fdbf9ac7cc3206e6b71d1ede5b13f2e5d5f3af60
Evgeni <666997@bk.ru>
2022-04-17 13:44:49 +0800

    Задача 3

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿int number = 165465465;
+int index = 1;
+int a = 1;
+int number2 = number;
+if(number / 100 >0)
+{
+    while (number > 10)
+    {
+        index = index + 1;
+        number = number / 10;
+    }
+    while (index > 3)
+    {
+        a = a * 10;
+        index = index - 1;
+    }
+    Console.WriteLine($"третья цифра {number2 / a % 10}");
+}
+else
+{
+    Console.WriteLine("нет третьей цифры");
+}
